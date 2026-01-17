f6654833fa81bea3d9e5064df31d44eb80103512
MAR PD <mdranasheikhe2005@gmail.com>
2026-01-18 04:08:19 +0600

    Remove get_logger from utils package exports

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,12 +2,11 @@
 Utils package for Roastify Bot
 """
 
-from .logger import logger, get_logger, log_info, log_error, log_warning, log_debug
+from .logger import logger, log_info, log_error, log_warning, log_debug
 
 __all__ = [
     'logger',
-    'get_logger',
-    'log_info',
+    'log_info', 
     'log_error',
     'log_warning',
     'log_debug'
